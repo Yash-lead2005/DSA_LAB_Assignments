30b2ea107d311bbeaf64d5254efcc1a697dc800a
Yash-lead2005 <www.yashjain.yj123@gmail.com>
2025-11-26 14:18:39 +0530

    ad1

--- Assignment_9/Add_1.cpp
+++ Assignment_9/Add_1.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void dfs(int node, vector<int> adj[], vector<int> &visited) {
+int adj[100][100];    
+int visited[100];     
+
+int V, E;
+
+void dfs(int node) {
     visited[node] = 1;
-    for(int neighbor : adj[node]) {
-        if(!visited[neighbor])
-            dfs(neighbor, adj, visited);
+
+    for (int i = 0; i < V; i++) {
+        if (adj[node][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
     }
 }
 
 int main() {
-    int V, E;
     cin >> V >> E;
 
-    vector<int> adj[V];
-    for(int i = 0; i < E; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    vector<int> visited(V, 0);
-    int components = 0;
-
-    for(int i = 0; i < V; i++) {
-        if(!visited[i]) {
-            components++;
-            dfs(i, adj, visited);
+    for (int i = 0; i < V; i++) {
+        visited[i] = 0;
+        for (int j = 0; j < V; j++) {
+            adj[i][j] = 0;
         }
     }
 
-    cout << components << endl;
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1; 
+    }
+
+    int components = 0;
+
+    for (int i = 0; i < V; i++) {
+        if (visited[i] == 0) {
+            components++;
+            dfs(i);
+        }
+    }
+    cout << components;
+
     return 0;
 }
